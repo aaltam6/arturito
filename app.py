100eb8e2065e7348210a11c9961f6219f61ac4d1
aaltam6 <aaltam6@lsu.edu>
2024-06-01 19:41:35 -0500

    html test

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("hello world")
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+         return render_template('home.html')
+
+    else:
+            return render_template('home.html')
