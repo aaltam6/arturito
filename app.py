4486215776c813d97008e649d3ec7cac86622132
aaltam6 <aaltam6@lsu.edu>
2024-06-15 19:52:37 -0500

    add

--- app.py
+++ app.py
@@ -4,8 +4,21 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-         return render_template('home.html')
+     if request.method == 'POST':
+          return render_template('home.html')
+     else:
+          return render_template('home.html')
 
+@app.route('/experience', methods=['GET', 'POST'])
+def experience():
+    if request.method == 'POST':
+         return render_template('experience.html')
     else:
-            return render_template('home.html')
+          return render_template('experience.html')
+    
+@app.route('/education', methods=['GET', 'POST'])
+def education():
+    if request.method == 'POST':
+         return render_template('education.html')
+    else:
+          return render_template('education.html')
